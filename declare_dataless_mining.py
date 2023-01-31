2a0cbdcaff88dc96b1c004d501ad8a68235cada1
Samuel Appleby <sambuzzappleby@hotmail.co.uk>
2023-01-31 17:02:48 +0000

    Latest

--- declare_dataless_mining.py
+++ declare_dataless_mining.py
@@ -14,9 +14,9 @@ def DeclareDataless_mining(log_path, filterCandidates=True, candidate_threshold=
         writer = csv.writer(f)
 
         if f.tell() == 0:
-            header = ['log_filename', 'mining_algorithm', 'n_traces', 'log_trace_average_length', 'min_support', 'iteration_num',
-                      'loading_and_indexing_time', 'query_time']
-            writer.writerow(header)
+            writer.writerow(['log_filename', 'mining_algorithm', 'n_traces', 'log_trace_average_length', 'min_support',
+                             'iteration_num',
+                             'loading_and_indexing_time', 'query_time'])
 
         obj = DeclareDevMining()
 
@@ -36,8 +36,9 @@ def DeclareDataless_mining(log_path, filterCandidates=True, candidate_threshold=
         end = time.time()
         mining_time = (end - start) * 1000
 
-        result = [log_path, 'ADM_S', len(log.traces), log.max_length, candidate_threshold, iteration_num, loading_time, mining_time]
-        writer.writerow(result)
+        writer.writerow(
+            [log_path, 'ADM_S', len(log.traces), log.max_length, candidate_threshold, iteration_num, loading_time,
+             mining_time])
         print("MaxSat")
         training = training.sparse.to_dense()
         for col in training.columns:
@@ -46,6 +47,20 @@ def DeclareDataless_mining(log_path, filterCandidates=True, candidate_threshold=
         sum = training.sum(axis=1)
         print(sum)
 
+        # sum1 = training.sum(axis=0)
+        # sum1 /= len(sum.index)
+        # # sum1 = sum1[sum1 == 1]
+        # print(sum1)
+        #
+        # with open("/home/sam/Documents/Repositories/CodeBases/ModelMining/mined_clauses.csv", 'a',
+        #           encoding='UTF8') as f1:
+        #     writer1 = csv.writer(f1)
+        #
+        #     if f1.tell() == 0:
+        #         writer1.writerow(['algorithm', 'clause', 'support'])
+        #
+        #     for result in sum1.index:
+        #         writer1.writerow(['ADM_S', result, sum1[result]])
 
 
 if __name__ == '__main__':
@@ -59,8 +74,10 @@ if __name__ == '__main__':
     parser.add_argument('-o', '--output', required=True, action='store',
                         type=str, dest='output_file', default=False, help="provide directory for csv outputting")
     parser.add_argument('-i', '--iterations', required=True, action='store',
-                        type=int, dest='num_iters', default=False, help="provide number of iterations for the algorithm to run")
+                        type=int, dest='num_iters', default=False,
+                        help="provide number of iterations for the algorithm to run")
     args = parser.parse_args()
 
     for i in range(args.num_iters):
-        DeclareDataless_mining(log_path=args.log_file, candidate_threshold=args.support, constraint_threshold=0, output_file=args.output_file, iteration_num=i)
+        DeclareDataless_mining(log_path=args.log_file, candidate_threshold=args.support, constraint_threshold=0,
+                               output_file=args.output_file, iteration_num=i)
