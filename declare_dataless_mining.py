8f9b843e06543eed204bb1f0359d39ec5c04957b
SamuelAppleby <61326361+SamuelAppleby@users.noreply.github.com>
2023-04-05 16:55:31 +0100

    Updated mining scripts.

--- declare_dataless_mining.py
+++ declare_dataless_mining.py
@@ -10,13 +10,8 @@ from embeddings.declare.DatalessDeclareEmbedding import DeclareDevMining
 
 def DeclareDataless_mining(log_path, filterCandidates=True, candidate_threshold=0.1, constraint_threshold=0.1,
                            withData=False, output_file="", iteration_num=0):
-    with open(output_file, 'a', encoding='UTF8') as f:
+    with open(output_file, 'a+', encoding='UTF8') as f:
         writer = csv.writer(f)
-
-        if f.tell() == 0:
-            writer.writerow(['log_filename', 'mining_algorithm', 'n_traces', 'log_trace_average_length', 'min_support',
-                             'iteration_num',
-                             'loading_and_indexing_time', 'query_time'])
 
         obj = DeclareDevMining()
 
@@ -36,31 +31,39 @@ def DeclareDataless_mining(log_path, filterCandidates=True, candidate_threshold=
         end = time.time()
         mining_time = (end - start) * 1000
 
-        writer.writerow(
-            [log_path, 'ADM_S', len(log.traces), log.max_length, candidate_threshold, iteration_num, loading_time,
-             mining_time])
-        print("MaxSat")
-        training = training.sparse.to_dense()
-        for col in training.columns:
-            training.loc[training[col] >= 0, col] = 1
-            training.loc[training[col] < 0, col] = 0
-        sum = training.sum(axis=1)
-        print(sum)
+        if iteration_num != 0:
+            if f.tell() == 0:
+                writer.writerow(
+                    ['log_filename', 'mining_algorithm', 'n_traces', 'log_trace_average_length', 'min_support',
+                     'iteration_num',
+                     'loading_and_indexing_time', 'query_time'])
 
-        sum1 = training.sum(axis=0)
-        sum1 /= len(sum.index)
-        # sum1 = sum1[sum1 == 1]
-        # print(sum1)
+            writer.writerow(
+                [log_path, 'ADM_S', len(log.traces), log.max_length, candidate_threshold, iteration_num, loading_time,
+                 mining_time])
+
+        # print("MaxSat")
+        # training = training.sparse.to_dense()
+        # for col in training.columns:
+        #     training.loc[training[col] >= 0, col] = 1
+        #     training.loc[training[col] < 0, col] = 0
+        # sum = training.sum(axis=1)
+        # print(sum)
+
+        # sum1 = training.sum(axis=0)
+        # sum1 /= len(sum.index)
+        # # sum1 = sum1[sum1 == 1]
+        # # print(sum1)
+        # #
+        # with open("/home/sam/Documents/Repositories/Codebases/ModelMining/mined_clauses.csv", 'a',
+        #            encoding='UTF8') as f1:
+        #      writer1 = csv.writer(f1)
         #
-        with open("/home/sam/Documents/Repositories/CodeBases/ModelMining/mined_clauses.csv", 'a',
-                   encoding='UTF8') as f1:
-             writer1 = csv.writer(f1)
-        
-             if f1.tell() == 0:
-                 writer1.writerow(['algorithm', 'clause', 'support'])
-        
-             for result in sum1.index:
-                 writer1.writerow(['ADM_S', result, sum1[result]])
+        #      if f1.tell() == 0:
+        #          writer1.writerow(['algorithm', 'clause', 'support'])
+        #
+        #      for result in sum1.index:
+        #          writer1.writerow(['ADM_S', result, sum1[result]])
 
 
 if __name__ == '__main__':
@@ -78,6 +81,6 @@ if __name__ == '__main__':
                         help="provide number of iterations for the algorithm to run")
     args = parser.parse_args()
 
-    for i in range(args.num_iters):
+    for i in range(args.num_iters + 1):
         DeclareDataless_mining(log_path=args.log_file, candidate_threshold=args.support, constraint_threshold=0,
                                output_file=args.output_file, iteration_num=i)
